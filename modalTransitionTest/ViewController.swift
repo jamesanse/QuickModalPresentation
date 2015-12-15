9680acb89399beef8f3ece94ea0338376656a5d3
james an <jamesanse@gmail.com>
2015-12-15 13:13:15 +0800

    Initial Commit

---
+++ modalTransitionTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  modalTransitionTest
+//
+//  Created by James An on 12/15/15.
+//  Copyright © 2015 Jamesan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
